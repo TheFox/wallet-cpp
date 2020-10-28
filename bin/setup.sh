5102781f5840b63d586190e1669fc088a15e967b
Christian Mayer <christian@fox21.at>
2020-10-28 15:19:54 +0100

    Install dev requirements for macOS environment.

--- bin/setup.sh
+++ bin/setup.sh
@@ -10,8 +10,12 @@ if [[ ! -f .env ]]; then
 fi
 
 kernel_name=$(uname -s)
+echo "Kernel: ${kernel_name}"
+
 if [[ "${kernel_name}" = Linux ]]; then
 	# lsb_release -i -s
 	# Assumes Debian or Ubuntu.
 	sudo apt-get install -y --no-install-recommends libyaml-cpp-dev libboost-filesystem-dev libboost-program-options-dev libboost-date-time-dev
+elif [[ "${kernel_name}" = Darwin ]]; then
+    brew install boost catch2 termcolor
 fi
